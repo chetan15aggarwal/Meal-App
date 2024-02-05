34558118d27781da8adbcf0a601d53a5c88903b9
Chetan Aggarwal <chetan15aggarwal@gmail.com>
2024-02-05 15:48:41 +0100

    `HTTPClient` and `URLSessionHTTPCleint` added with its test cases

---
+++ Meal-App/Network/HTTPClient.swift
@@ -0,0 +1,16 @@
+//
+// Meal-App
+// Created by Chetan Aggarwal.
+
+
+import Foundation
+
+enum HTTPClientResult {
+    case success(Data, HTTPURLResponse)
+    case failure(Error)
+}
+
+// MARK: - HTTPClient Protocol
+protocol HTTPClient {
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void )
+}
