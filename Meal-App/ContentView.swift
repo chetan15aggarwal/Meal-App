2e90bc23fe8339bfb5cb6023e8549f29f5b45576
Chetan Aggarwal <chetan15aggarwal@gmail.com>
2024-02-05 14:58:11 +0100

    initial commit

---
+++ Meal-App/ContentView.swift
@@ -0,0 +1,22 @@
+//
+// Meal-App
+// Created by Chetan Aggarwal.
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Meal-App/Meal_AppApp.swift
@@ -0,0 +1,15 @@
+//
+// Meal-App
+// Created by Chetan Aggarwal.
+
+
+import SwiftUI
+
+@main
+struct Meal_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
